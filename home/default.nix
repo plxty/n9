d0062c28b41b0dbfe585eec94c71096bb8099340
Zigit Zo <repl@z.xas.is>
2025-02-27 10:36:41 +0800

    stash: to lib

--- home/default.nix
+++
@@ -1,6 +0,0 @@
-args: {
-  desktop.pop-shell = import ./desktop/pop-shell.nix args;
-  v12n.boxes = import ./v12n/boxes.nix;
-  miscell.git = import ./miscell/git.nix;
-  miscell.ssh = import ./miscell/ssh.nix args;
-}
