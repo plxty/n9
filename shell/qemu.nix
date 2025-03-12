1fc4d59eb29b16ed765740b3964154dbd3567883
Zigit Zo <repl@z.xas.is>
2025-03-12 15:03:47 +0800

    fix(shell): qemu works

--- shell/qemu.nix
+++ shell/qemu.nix
@@ -1,5 +1,11 @@
 { system, nixpkgs, ... }:
 
+# The cross.pkgs.stdenv will handle (build,host,target) well, and we just use it:
+# https://github.com/NixOS/nixpkgs/blob/master/pkgs/stdenv/cross/default.nix
+# @see NixOS Reference: Dependency propagation
+# build: current the platform, host: the cross compile toolchains, target: eventually runs at
+# https://github.com/NixOS/nixpkgs/issues/35543
+
 let
   target = "aarch64-linux";
 
@@ -13,8 +19,7 @@ let
     else
       {
         aarch64-linux = {
-          # eq to 'pkgs = import <nixpkgs> { crossSystem.config = "..."; };'
-          # It seems the static QEMU is using musl-c, we're trying to keep it up:
+          # eq. 'pkgs = import <nixpkgs> { crossSystem.config = "..."; };'
           pkgs = pkgs.pkgsCross.aarch64-multiplatform-musl;
           configure = "--cross-prefix=aarch64-unknown-linux-musl-";
         };
@@ -40,6 +45,8 @@ let
     cd build
 
     # MUST disable split-debug to workaround ASM_FINAL_SPEC in GCC that trying to run native objcopy:
+    # (The split-debug may still in development, it occurs in the latest QEMU commit.)
+    # GIO has problem with static link, causing redefinition of crc32c.
     ../configure \
       --target-list=${targetList} \
       ${cross.configure} \
@@ -48,6 +55,7 @@ let
       --disable-split-debug \
       --disable-tools \
       --disable-guest-agent \
+      --disable-gio \
       --enable-kvm \
       --enable-linux-io-uring \
       --enable-vhost-net \
@@ -60,37 +68,29 @@ let
     make -C build -j $(nproc --ignore=2) "$@"
   '';
 in
-# https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/virtualization/qemu/default.nix
-# https://nixos.wiki/wiki/Cross_Compiling
-# The cross.pkgs.stdenv will handle (build,host,target) well, and we just use it:
-# https://github.com/NixOS/nixpkgs/blob/master/pkgs/stdenv/cross/default.nix
 cross.pkgs.mkShell {
   name = "qemu";
 
-  # Making dependent libraries static:
-  inputsFrom = with cross.pkgs.pkgsStatic; [
-    # Keep it simple to reduce dependency build time in nix:
-    (qemu.override { minimal = true; })
-    # The glib requires some of the depencies be installed:
+  # Like configure or some code-gen tools that runs on build:
+  depsBuildBuild = with pkgs; [
+    meson
+    ninja
+    python3
+    gcc # needs for hexagon
+  ];
+
+  # Mostly the cross compilers, kind of similar to depsBuildBuild:
+  nativeBuildInputs = with cross.pkgs.buildPackages; [
+    gcc
+    pkg-config
+  ];
+
+  # Making dependent libraries static, pkgsStatic.qemu is broken, and won't work
+  # event { minimal = true; }, causing the inputsFrom a little bit awkward.
+  buildInputs = with cross.pkgs.pkgsStatic; [
     glib
+    liburing
   ];
-
-  # In cross compile (in my own opinion),
-  # build: current the platform
-  # host: the cross compile toolchains
-  # target: eventually runs at
-  depsBuildBuild = [
-    # QEMU needs for hexagon:
-    pkgs.gcc
-  ];
-
-  # https://discourse.nixos.org/t/use-buildinputs-or-nativebuildinputs-for-nix-shell/8464
-  # @see NixOS Reference: Dependency propagation
-  # buildInputs = with cross.pkgs; [
-  #   # Need both dynamic (for meson configure) and static (for building to target) glibc:
-  #   glibc
-  #   glibc.static
-  # ];
 
   packages = [
     configure
