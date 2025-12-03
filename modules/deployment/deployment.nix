60c4790a3500cc88c46ce1c10b7c6d0dbf089516
Zigit Zo <byte@kei.network>
2025-12-03 16:14:11 +0800

    pkgs: colmena: supports activateScript and switchGoal

--- modules/deployment/deployment.nix
+++ modules/deployment/deployment.nix
@@ -11,17 +11,22 @@
   options.deployment = {
     profileWide = lib.mkOption {
       type = lib.types.str;
-      default =
-        if config.variant.is.home-manager then
-          # For home-manager, we should use a different directory to make colmena works:
-          "${config.variant.home-manager.home.homeDirectory}/.local/state/nix/profiles/home-manager"
-        else
-          "/nix/var/nix/profiles/system";
+      default = "/nix/var/nix/profiles/system"; # nixos && nix-darwin
     };
 
     profileCurrent = lib.mkOption {
       type = lib.types.str;
       default = "/run/current-system";
+    };
+
+    activateScript = lib.mkOption {
+      type = lib.types.str;
+      default = "bin/switch-to-configuration"; # nixos
+    };
+
+    switchGoal = lib.mkOption {
+      type = lib.types.nullOr (lib.types.listOf lib.types.str);
+      default = null;
     };
 
     rootAbsolute = lib.mkOption {
@@ -30,8 +35,23 @@
     };
   };
 
-  config.deployment = lib.mkIf config.variant.is.home-manager {
-    privilegeEscalationCommand = [ ];
-    targetUser = config.variant.home-manager.home.username;
-  };
+  config.deployment = lib.mkMerge [
+    (lib.mkIf config.variant.is.home-manager {
+      privilegeEscalationCommand = [ ];
+      targetUser = config.variant.home-manager.home.username;
+
+      # For home-manager, we should use a different directory to make colmena works:
+      profileWide = "${config.variant.home-manager.home.homeDirectory}/.local/state/nix/profiles/home-manager";
+      activateScript = "activate";
+      switchGoal = [
+        "--driver-version"
+        "1"
+      ];
+    })
+
+    (lib.mkIf config.variant.is.nix-darwin {
+      activateScript = "activate";
+      switchGoal = [ ];
+    })
+  ];
 }
